16ff9ad6eb4c21c496a9e476965fe6ad159bad6d
Elton Memishaj <eltoni.memishi@live.com>
2021-05-02 22:30:00 +0200

    Add "is loading" example on CategoriesComponent

--- src/app/home/categories/categories.component.ts
+++ src/app/home/categories/categories.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { finalize, take, takeUntil } from 'rxjs/operators';
 import { ICategory } from 'src/app/core/models/category';
 import { CategoryService } from 'src/app/core/services/category.service';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
@@ -16,6 +16,7 @@ import { CategoriesCreateUpdateComponent } from './categories-create-update/cate
 export class CategoriesComponent implements OnInit, OnDestroy {
   private onComponentDestroy$: Subject<void> = new Subject<void>();
   public categories!: Array<ICategory>;
+  public isLoading!: boolean;
   // public categories$!: Observable<Array<ICategory>>;
 
   constructor(
@@ -34,44 +35,57 @@ export class CategoriesComponent implements OnInit, OnDestroy {
   }
 
   public loadCategories(): void {
+    this.isLoading = true;
     this.categoryService.getList().pipe(
       take(1),
-      takeUntil(this.onComponentDestroy$)
-    ).subscribe((response: Array<ICategory>) => this.categories = response);
+      takeUntil(this.onComponentDestroy$),
+      finalize(() => this.isLoading = false)
+    ).subscribe(
+      (response: Array<ICategory>) => this.categories = response,
+      () => this.matSnackBar.open('Error loading Category list!', 'Dismiss', { duration: 0, panelClass: [ 'warn-background', 'white-color' ] })
+    );
     // this.categories$ = this.categoryService.getList();
   }
 
   public createCategory(): void {
     const dialogRef: MatDialogRef<CategoriesCreateUpdateComponent> = this.dialog.open(CategoriesCreateUpdateComponent);
     dialogRef.componentInstance.onSaveFn = (formValue: ICategory) => {
-      this.categoryService.create(formValue).pipe(
-        take(1),
-        takeUntil(this.onComponentDestroy$)
-      ).subscribe(
-        (response: ICategory) => {
-          dialogRef.close();
-          this.categories.push(response);
-          this.matSnackBar.open(`Category "${response.name}" created successfully!`);
-        },
-        () => this.matSnackBar.open(`There was an error creating category "${formValue.name}"!`)
-      );
+      if (this.categories?.find((category: ICategory) => category.name?.trim()?.toLowerCase() === formValue.name?.trim()?.toLowerCase())) {
+        this.matSnackBar.open(`Category with name "${formValue.name}" already exists!`);
+      } else {
+        this.categoryService.create(formValue).pipe(
+          take(1),
+          takeUntil(this.onComponentDestroy$)
+        ).subscribe(
+          (response: ICategory) => {
+            dialogRef.close();
+            this.categories.push(response);
+            this.matSnackBar.open(`Category "${response.name}" created successfully!`);
+          },
+          () => this.matSnackBar.open(`There was an error creating category "${formValue.name}"!`)
+        );
+      }
     };
   }
 
   public updateCategory(category: ICategory, index: number): void {
     const dialogRef: MatDialogRef<CategoriesCreateUpdateComponent> = this.dialog.open(CategoriesCreateUpdateComponent, { data: category });
     dialogRef.componentInstance.onSaveFn = (formValue: ICategory) => { // Rishikojme
-      this.categoryService.update({ ...category, ...formValue }).pipe(
-        take(1),
-        takeUntil(this.onComponentDestroy$)
-      ).subscribe(
-        (response: ICategory) => {
-          dialogRef.close();
-          this.categories[index] = response;
-          this.matSnackBar.open(`Category "${category.name}" updated successfully to "${response.name}"!`);
-        },
-        () => this.matSnackBar.open(`There was an error updating category "${category.name}" to "${formValue.name}"!`)
-      );
+      if (this.categories?.find((categoryFromList: ICategory) => categoryFromList.name?.trim()?.toLowerCase() === formValue.name?.trim()?.toLowerCase())) {
+        this.matSnackBar.open(`Category with name "${formValue.name}" already exists!`);
+      } else {
+        this.categoryService.update({ ...category, ...formValue }).pipe(
+          take(1),
+          takeUntil(this.onComponentDestroy$)
+        ).subscribe(
+          (response: ICategory) => {
+            dialogRef.close();
+            this.categories[index] = response;
+            this.matSnackBar.open(`Category "${category.name}" updated successfully to "${response.name}"!`);
+          },
+          () => this.matSnackBar.open(`There was an error updating category "${category.name}" to "${formValue.name}"!`)
+        );
+      }
     };
   }
 

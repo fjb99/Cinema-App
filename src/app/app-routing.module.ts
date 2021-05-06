30c58923399c984b54bfd598048488fd90612b97
Elton Memishaj <eltoni.memishi@live.com>
2021-05-06 21:54:11 +0200

    Remove `PageNotFoundComponent` and replace `**` route to redirect to "home" (or login if not logged in)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth.guard';
 import { LoginComponent } from './core/auth/login/login.component';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard], loadChildren: () => import ('./home/home.module').then((m) => m.HomeModule) },
-  { path: '**', component: PageNotFoundComponent }
-
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent,
     ConfirmDialogComponent
   ],
   imports: [
@@ -16,7 +14,6 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatDialogModule
   ],
   exports: [
-    PageNotFoundComponent,
     ConfirmDialogComponent
   ]
 })

63ceff12ee273ef13fbcbb8cd0bee75bb30e9be6
Elton Memishaj <eltoni.memishi@live.com>
2021-05-07 23:04:47 +0200

    Add environment variables on environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  /**
+   * Do not add trailing slash, add slash during usage, instead
+   * @example return this.httpClient.get<Array<IMovie>>(`${environment.apiUrl}/movies`)
+   */
+  apiUrl: 'https://607b2b11bd56a60017ba3708.mockapi.io/api',
+  loginApiUrl: 'https://reqres.in/api/login',
+  loggedInUserLocalStorageKey: 'internshipProjectLoggedInUserData'
 };
